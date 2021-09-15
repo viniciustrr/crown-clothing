9a8a2523b5acc073e7f48a3d4a1adfb44461cbe8
Vinicius Trindade <vinicius.ribeiro@ccc.ufcg.edu.br>
2021-09-15 13:00:55 -0300

    Comeco de rotas de navegacao e pagina shops feita

--- src/components/directory/directory-component.jsx
+++ src/components/directory/directory-component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MenuItem from "../menu-item/menu-item-component";
 import './directory-style.scss';
 
+
 class Directory extends React.Component {
     constructor(){
         super();
@@ -47,8 +48,8 @@ class Directory extends React.Component {
         return(
             <div className = 'directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl,id, size}) => (
-                    <MenuItem key = {id} title = {title} imageUrl = {imageUrl} size = {size} />
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                    <MenuItem key = {id} {...otherSectionProps} />
                     ))
                 }
             </div>
@@ -57,4 +58,4 @@ class Directory extends React.Component {
 
 }
 
-export default Directory;
+export default Directory ;
