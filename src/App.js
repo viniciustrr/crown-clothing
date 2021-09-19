15a092ea5e6fafca499e68508e6dcd41744c8279
Vinicius Trindade <vinicius.ribeiro@ccc.ufcg.edu.br>
2021-09-19 16:27:36 -0300

    Header feito e componente de botao

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { Route, Switch } from 'react-router';
 import './App.css';
 import HomePage from './pages/homepage/homepage-component';
 import ShopPage from './pages/shop/shop-component';
-
+import Header from './components/header/header-component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up-component';
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route  exact path = '/' component = {HomePage} />
         <Route  exact path = '/shop' component = {ShopPage} />
+        <Route  exact path = '/signin' component = {SignInAndSignUpPage} />
       </Switch>
     </div>
   );
