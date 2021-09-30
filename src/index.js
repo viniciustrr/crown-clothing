5ce3649033d74cb76b45d6a8fc36c455cb4521d7
Vinicius Trindade <vinicius.ribeiro@ccc.ufcg.edu.br>
2021-09-30 18:44:51 -0300

    adicionei persistencia para salvar os dados localmente

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import {Provider} from 'react-redux';
-import store from './redux/store'
+import {store, persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+
+
+
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App/>
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
