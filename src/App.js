db42423b7b169a9aa7e07b936739ed2616f569bd
Vinicius Trindade <vinicius.ribeiro@ccc.ufcg.edu.br>
2021-10-08 10:55:09 -0300

    Mandei dados do shop_data para o firestore - falta implementar a utilização desses dados de la

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import CheckoutPage from './pages/checkout/checkout-component';
 
 
 
+
 class App extends React.Component {
 
 
@@ -21,7 +22,7 @@ class App extends React.Component {
 
   componentDidMount(){
 
-    const {setCurrentUser} = this.props;
+    const {setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
@@ -64,11 +65,12 @@ class App extends React.Component {
 
 
 const mapStateToProps = createStructuredSelector({
-    currentUser: selectCurrentUser
+    currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
+  
 });
 
 export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/firebase/firebase-utils.js
+++ src/firebase/firebase-utils.js
@@ -40,6 +40,22 @@ const config = {
 
   firebase.initializeApp(config);
 
+
+  export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd
+  ) => {
+    const collectionRef = firestore.collection(collectionKey);
+  
+    const batch = firestore.batch();
+    objectsToAdd.forEach((obj) => {
+      const newDocRef = collectionRef.doc();
+      batch.set(newDocRef, obj);
+    });
+  
+    return await batch.commit();
+  };
+
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
 
