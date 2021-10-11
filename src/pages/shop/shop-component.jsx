06c42ecf5f90217caada71b933ec81262fba2a91
Vinicius Trindade <vinicius.ribeiro@ccc.ufcg.edu.br>
2021-10-11 11:42:15 -0300

    adicionei o reducer do shop collections e fiz com que atraves de uma acao o sistema pegasse os dados do firebase e mapeasse para um formato utilizavel

--- src/pages/shop/shop-component.jsx
+++ src/pages/shop/shop-component.jsx
@@ -2,15 +2,41 @@ import React from "react";
 import CollectionsOverview from "../../components/collections-overview/collections-overview-component";
 import { Route } from "react-router-dom";
 import CollectionPage from "../collection/collection-component";
+import { firestore,convertCollectionsSnapshotToMap } from "../../firebase/firebase-utils";
+import { updateCollections } from "../../redux/shop/shop-actions";
+import { connect } from "react-redux";
 
 
-const ShopPage = ({match}) => (
-    <div >
-        <Route exact path = {`${match.path}`} component={CollectionsOverview}/>
-        <Route path ={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>   
-);
+class ShopPage extends React.Component {
+
+    unsubscribeFromSnapshot = null;
+
+    componentDidMount(){
+        const {updateCollections} = this.props;
+        const collectionRef = firestore.collection('collections');
+        collectionRef.onSnapshot(async snapshot => {
+            const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionMap);
+        });
+    }
 
 
+    render(){
+        
+        const {match} = this.props;
+        return (
+            <div >
+                <Route exact path = {`${match.path}`} component={CollectionsOverview}/>
+                <Route path ={`${match.path}/:collectionId`} component={CollectionPage} />
+            </div>   
+        );
+        
+    }
+} 
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
+
+
+export default connect(null,mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop-reducer.js
+++ src/redux/shop/shop-reducer.js
@@ -1,4 +1,5 @@
 import SHOP_DATA from "./shop.data";
+import ShopActionTypes from "./shop-types";
 
 
 const INITIAL_STATE = {
@@ -7,6 +8,13 @@ const INITIAL_STATE = {
 
 const shopReducer  = (state = INITIAL_STATE, action) => {
     switch(action.type){
+
+        case ShopActionTypes.UPDATE_COLLECTIONS:
+            return {
+                ...state,
+                collections: action.payload
+
+            }
         default:
            return state; 
     }
