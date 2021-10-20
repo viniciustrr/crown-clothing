b19b78d1fa60140c32a151a145862b2eeb8a87fd
Vinicius Trindade <vinicius.ribeiro@ccc.ufcg.edu.br>
2021-10-20 11:41:01 -0300

    Limpando shop actions

--- src/redux/shop/shop-actions.js
+++ src/redux/shop/shop-actions.js
@@ -1,5 +1,4 @@
 import ShopActionTypes from "./shop-types";
-import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase-utils';
 
 export const fetchCollectionsStart = () => ({
     type: ShopActionTypes.FETCH_COLLECTIONS_START,
@@ -15,15 +14,5 @@ export const fetchCollectionsFailure = errorMessage => ({
     payload: errorMessage
 });
 
-export const fetchCollectionsStartAsync = () => {
-    return dispatch => {
-        const collectionRef = firestore.collection('collections');
-        dispatch(fetchCollectionsStart());
 
-        collectionRef.get().then(snapshot => {
-          const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-          dispatch(fetchCollectionsSuccess(collectionsMap));
-        }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
-    }
-};
 
